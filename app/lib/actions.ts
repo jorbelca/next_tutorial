4d05a1ea9763ca92ecd0709d99f8e4f0be8007aa
j <76847923+jorbelca@users.noreply.github.com>
2024-12-12 11:24:16 +0100

    corrections for chapter 12

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -15,7 +15,7 @@ const FormSchema = z.object({
 const CreateInvoice = FormSchema.omit({ id: true, date: true });
 const UpdateInvoice = FormSchema.omit({ id: true, date: true });
 
-export async function createInvoice(formData: FormData) {
+export async function createInvoice(formData: FormData): Promise<void> {
   const { customerId, amount, status } = CreateInvoice.parse({
     customerId: formData.get("customerId"),
     amount: formData.get("amount"),
@@ -23,13 +23,17 @@ export async function createInvoice(formData: FormData) {
   });
   const amountInCents = amount * 100;
   const date = new Date().toISOString().split("T")[0];
+
   try {
     await sql`
   INSERT INTO invoices (customer_id, amount, status, date)
   VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
 `;
   } catch (error) {
-    return { message: "Database Error: Failed to Create Invoice." };
+    {
+      message: "Database Error: Failed to Create Invoice.";
+    }
+    return;
   }
   revalidatePath("/dashboard/invoices");
   redirect("/dashboard/invoices");
@@ -57,7 +61,7 @@ export async function updateInvoice(id: string, formData: FormData) {
   redirect("/dashboard/invoices");
 }
 
-export async function deleteInvoice(id: string) {
+export async function deleteInvoice(id: string): Promise<{ message: string }> {
   try {
     await sql`DELETE FROM invoices WHERE id = ${id}`;
     revalidatePath("/dashboard/invoices");
